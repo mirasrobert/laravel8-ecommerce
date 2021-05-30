a28a7dc099d22da9b032809c5c4e7cc2b6b351c5
mirasrobert <mrmirasrobert@gmail.com>
2021-05-30 20:40:18 +0800

    File Upload (2)

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -60,7 +60,7 @@ class ProductController extends Controller
         // Move uploaded file to the folder with FILE PATH and FILE NAME
         $imageNameWithPath = $request->image->store('product_img', 'public');
 
-        dd($imageNameWithPath);
+        dd(public_path($imageNameWithPath));
 
         // Make-Upload Image
         //$image = Image::make("storage/{$imageNameWithPath}")->resize(320, 300);
